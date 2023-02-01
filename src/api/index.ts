630153ddcff27f7eb14b4f0ad75538ae1fe50e09
sorious77 <sorious77@naver.com>
2023-02-01 22:33:31 +0900

    썸네일 받기 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,11 +4,26 @@ import stations from "./stations";
 import posts from "./posts";
 import lists from "./lists";
 
+import multer from "multer";
+
 const router = Router();
 
 router.use("/users", users);
 router.use("/stations", stations);
 router.use("/posts", posts);
 router.use("/lists", lists);
+
+const upload = multer({ dest: "uploads/" });
+
+router.post("/thumbnail", upload.single("image"), async (req, res) => {
+  console.log("Received image:", req.file);
+  res.json({ message: "Image uploaded successfully" });
+});
+
+router.get("/test", (req, res) => {
+  console.log("testtest");
+
+  res.json({ message: "test" });
+});
 
 export default router;
