9f22e502ca82ee884264c667bb43308ee0213970
sorious77 <sorious77@naver.com>
2023-07-24 23:00:12 +0900

    내가 쓴 글 api 구현

--- src/api/lists.ts
+++ src/api/lists.ts
@@ -4,6 +4,24 @@ import { PostService } from "../services/post.service";
 
 const router = Router();
 
+router.get("/myDiary", async (req: Request, res: Response) => {
+  const { page, email } = req.query as { page: string; email: string };
+
+  try {
+    if (!page || !email) throw new Error("page와 email은 필수입니다.");
+
+    const result = await PostService.getPostByPageAndEmail({
+      page: +page,
+      email,
+    });
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: "게시글 조회에 실패했습니다." });
+  }
+});
+
 router.get("/:page", async (req: Request, res: Response) => {
   const page = +req.params.page;
 
